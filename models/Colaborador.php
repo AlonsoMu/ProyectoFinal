561a5be8724d25e9a0e6fbaf3c0f19f9f3d84758
AlonsoMu <alonsomunoz263@gmail.com>
2023-04-21 14:17:18 -0600

    Rellenamos la carpeta models

---
+++ models/Colaborador.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Colaborador extends Conexion{
+
+  private $accesoBD;
+
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion();
+ }
+
+  public function eliminarColaborador($idcolaborador = 0){
+    try {
+      $consulta = $this->accesoBD->prepare("CALL spu_colaboradores_eliminar(?)");
+      $consulta->execute(array($idcolaborador));
+
+    }catch (Exception $e) {
+      die($e->getMessage());
+    }
+  }
+
+  public function registrarColaborador($datos = []){
+    try {
+        $consulta = $this->accesoBD->prepare("CALL spu_colaboradores_registrar(?,?,?,?,?,?,?,?)");
+        $consulta->execute(
+          array(
+            $datos['apellidos'], 
+            $datos['nombres'], 
+            $datos['idcargo'],
+            $datos['idsede'],
+            $datos['telefono'],
+            $datos['direccion'],
+            $datos['tipocontrato'],
+            $datos['cv']
+          )
+        );
+    }
+    catch (Exception $e) {
+    die($e->getMessage());
+    }
+  }
+
+  public function listarColaboradores(){
+    try{
+        $consulta = $this->accesoBD->prepare("CALL spu_colaboradores_listar()");
+        $consulta->execute();
+  
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+      }
+      catch(Exception $e){
+        die($e->getMessage());
+      }
+    }
+
+  public function obtenerColaborador($idcolaborador = 0){
+    try{
+        $consulta = $this->accesoBD->prepare("CALL spu_obtener_colaboradores(?)");
+        $consulta->execute(array($idcolaborador));
+  
+        return $consulta->fetch(PDO::FETCH_ASSOC);
+      }
+      catch(Exception $e){
+        die($e->getMessage());
+      }
+    }
+
+
+    
+}
+  
---
+++ models/Estudiante.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Estudiante extends Conexion{
+
+  private $accesoBD;
+
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion();
+  }
+
+  public function eliminarEstudiante($idestudiante = 0){
+    try {
+      $consulta = $this->accesoBD->prepare("CALL spu_estudiantes_eliminar(?)");
+      $consulta->execute(array($idestudiante));
+
+    }catch (Exception $e) {
+      die($e->getMessage());
+    }
+  }
+
+  //Datos[] es un array asociativo, que contiene la información
+  //a guardar proveniente del controlador
+  public function registrarEstudiante($datos = []){
+    try{
+      $consulta = $this->accesoBD->prepare("CALL spu_estudiantes_registrar(?,?,?,?,?,?,?,?)");
+      $consulta->execute(
+        array(
+          $datos['apellidos'], 
+          $datos['nombres'], 
+          $datos['tipodocumento'],
+          $datos['nrodocumento'],
+          $datos['fechanacimiento'],
+          $datos['idcarrera'],
+          $datos['idsede'],
+          $datos['fotografia']
+        )
+      );
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function listarEstudiantes(){
+    try{
+      $consulta = $this->accesoBD->prepare("CALL spu_estudiantes_listar()");
+      $consulta->execute();
+
+      return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function obtenerEstudiante($idestudiante = 0){
+    try{
+        $consulta = $this->accesoBD->prepare("CALL spu_obtener_estudiantes(?)");
+        $consulta->execute(array($idestudiante));
+  
+        return $consulta->fetch(PDO::FETCH_ASSOC);
+      }
+      catch(Exception $e){
+        die($e->getMessage());
+      }
+    }
+
+}
